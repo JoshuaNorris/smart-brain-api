c628f4b60caf85d79b5f052e8634688be741face
Joshua Norris <joshua.norris52@gmail.com>
2024-04-30 12:21:16 -0500

    db

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true
   }
 });
