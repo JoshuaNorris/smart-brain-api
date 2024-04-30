1b26c4648c6bc0f5ecd3e18656ebdcebff7c833c
Joshua Norris <joshua.norris52@gmail.com>
2024-04-30 15:25:25 -0500

    fixing api call

--- controllers/image.js
+++ controllers/image.js
@@ -1,5 +1,6 @@
 
 
+
 const handlePutImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
@@ -11,40 +12,60 @@ const handlePutImage = (req, res, db) => {
   .catch(err => res.status(400).json('unable to get entries'))
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const handleApiCall = (req, res) => {
-    const PAT = 'e53f23ed7b07471caf0bec6692c20a1d';
-    const USER_ID = 'norris52';       
-    const APP_ID = 'smart-brain';
-    const MODEL_ID = 'face-detection';
-    const IMAGE_URL = req.body.input;
-    const raw = JSON.stringify({
-      "user_app_id": {
+
+  const PAT = 'e53f23ed7b07471caf0bec6692c20a1d';
+  const USER_ID = 'norris52';
+  const APP_ID = 'smart-brain';
+  const MODEL_ID = 'face-detection';
+  const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+  const IMAGE_URL = req.body.input;
+  const raw = JSON.stringify({
+    "user_app_id": {
         "user_id": USER_ID,
         "app_id": APP_ID
-      },
-      "inputs": [
+    },
+    "inputs": [
         {
-          "data": {
-            "image": {
-              "url": IMAGE_URL
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                    // "base64": IMAGE_BYTES_STRING
+                }
             }
-          }
         }
-      ]
-    });
-    const requestOptions = {
-      method: 'POST',
-      headers: {
+    ]
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
         'Accept': 'application/json',
         'Authorization': 'Key ' + PAT
-      },
-      body: raw
-    };
-    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
-    .then(data => {
-      res.json(data);
+    },
+    body: raw
+  };
+
+  fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+        res.json(result);
     })
-    .catch(err => res.status(400).json('unable to work with api'))
+    .catch(error => console.log('error', error));
 }
 
 module.exports = {
